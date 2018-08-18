ea8058f10aa7d785d8b78a7f5e7ee2a79af60798
Vasja Markič <42490634+vasjamarkic@users.noreply.github.com>
2018-08-18 12:57:52 +0200

    Updated the code, with instructions
    Added info for wiring the device with the cell, charger, load and shunt.

--- SPI_OLED_NANO_battery_SOC_meter.ino
+++ SPI_OLED_NANO_battery_SOC_meter.ino
@@ -1,10 +1,8 @@
 /*********************************************************************
-Project DIY SOC meter for battery - using Arduino Nano and AdaFruit LCD 0.96"
+Project DIY SOC meter for battery cell/pack with Arduino Nano and AdaFruit LCD 0.96"
 Using the module 4SPI (look back the config of the OLED 128 x 64)
 
-Between the MOSFET IRF3205 (Gate and Ground) use 10k resistor
-
-Also in series put 2 x 4k6 resistor to analog input A0 in A1
+Also in series put 4k6 resistor to analog input A6 in A7
 measuring Udiff in shunt - it prevents damaging the Arduino
 while the batteries are pludged and Arduino is off. 
 
@@ -13,20 +11,33 @@ You can get the batteries out of the container and resume the discharge later.
 Credits to Adam: 
 --> YouTube Video: https://www.youtube.com/embed/qtws6VSIoYk
 --> http://AdamWelch.Uk
-A)
-adding 3 buttons for proframing the threshold voltage;
-UP/DOWN/SET. 
-First selecting the battLow voltage, increase/decrease by 0.1 V. 
-After pressing the SET button, select the battHigh voltage, incerase/decrease by 0.1 V.
-Press SET to confirm and start the measurments.
-B)
-Two poneciometers for battLow and battHigh treshold voltage, from 2 ÷ 12 V and 4 ÷ 16 V. 
-The tunning can be changed anytime while the circuit is taking measurments and the treshold values must be diplayed on the OLED. 
 
-Added the slave I2C protocol to send data to Arduino mega for graphic showing the SOC in Nextion display
-A4 (UNO, NANO) - 20 (MEGA)
-A5 (UNO, NANO) - 21 (MEGA), also common GND
-VasjArtigian 3.7.2018
+Adding 2 potencimeters for setting the threshold voltage and two buttons; left SET (black) the voltage, right RESET (red) delete the 
+data in eeprom. When the device is pludged to power (USB), after 5 sec the welcome display changes and the min and max voltages
+had to be selected, button SET confirms the selection displayed on the OLED.
+Two working modes:
+A) Measure only SOC with sending "High/low" state to other controllers
+B) Measure SOC and charging/discharging the cell with two relay integrated in the device
+
+When using mode A connect the green probes to the shunt resistor,the red to the +cell pole, the black to the -cell pole. 
+The yellow "line in" to the "HIGH/LOW" voltage, white probe bricks to "BAT 0%", yellow probe bricks to "BAT 100%".
+In mode B the yellow "line in" connect to the shunt resistor (same as green "A7"), yellow "Load" to +load pole, 
+white "V+" to +charger pole. Don't forget to connect the black probe to GND load and GND charger. 
+The shunt resistor is in sarial with the battery cell/pack.
+
+Connectors:
+White "V+" to +Charger pole OR send to controller as "bat 0%"
+Green "A6" and "A7" to shunt resistor
+Red "+" to +battery cell/pack pole
+Black to GND cell/battery pole AND GND charger and GND load
+Yellow "load" to +load pole OR send to controller as "bat 100%"
+Yellow "Line in" to shunt (same as green "A7") OR to HIGH/LOW source voltage, depending on the controller.
+
+Added the slave I2C protocol (wire.h) to send data to Arduino Mega for graphic showing the SOC in Nextion display, conetcion:
+A4 (UNO, NANO) - 20 (MEGA),
+A5 (UNO, NANO) - 21 (MEGA), 
+also common GND
+VasjArtigian 18.8.2018
 *********************************************************************/
 
 #include <SPI.h>
@@ -34,7 +45,7 @@ VasjArtigian 3.7.2018
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <EEPROM.h> 
-// For software SPI:
+// For software SPI - connection of the display to arduino nano:
 #define OLED_MOSI   11
 #define OLED_CLK   13
 #define OLED_DC    9
